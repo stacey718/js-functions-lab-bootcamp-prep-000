955668ed689b81010398f40ec270f67d683e9c4a
global-keyboard-5117 <stacey.cupid@gmail.com>
2018-10-09 01:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(Easter, Mommy) {
   return `Happy ${Easter}, ${Mommy}!`
 }
 function holidayCountdown(Easter, days) {
-  return `It\'s ${195} days until ${Easter}!`
+  return `It\'s ${20} days until ${Easter}!`
 }
